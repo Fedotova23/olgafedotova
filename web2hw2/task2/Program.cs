3bb64ae61eae5635f749e3dddd0323e3bce540f7
Liza <lizafedotova171197@gmail.com>
2023-02-06 23:29:32 +0300

    hw2

---
+++ web2hw2/task2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digits = 0;
+int tempNumber = number;
+int result = number;
+
+while (tempNumber > 0)
+{
+    tempNumber = tempNumber / 10;
+    digits++;
+};
+
+if (digits > 2){
+        while (digits > 3)
+    {
+    result = result/10;
+    digits--;
+    };
+    System.Console.WriteLine($"{number} -> {result % 10}"); 
+}
+else
+{
+    System.Console.WriteLine($"{number} -> третьей цифры нет");
+};
---
+++ web2hw2/task3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0 && number < 8)
+{ 
+        if (number < 6)
+    {
+    System.Console.WriteLine($"{number} -> нет");
+    }
+        else
+    {
+    System.Console.WriteLine($"{number} -> да");
+    };
+}
+else
+{
+    System.Console.WriteLine($"{number} -> такого дня нет");
+};
