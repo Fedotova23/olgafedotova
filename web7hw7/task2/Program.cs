4cf207cb20a40ae06db2e05a68a005a67563e40a
Liza <lizafedotova171197@gmail.com>
2023-02-24 14:33:55 +0300

    hw7

---
+++ web7hw7/task2/Program.cs
@@ -0,0 +1,65 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции
+ элемента в двумерном массиве, и возвращает значение этого элемента 
+ или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет*/
+
+System.Console.WriteLine("Введите количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите позицию элемента:");
+int position = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Generate2DArray(m,n);
+Print2DArray(array);
+System.Console.WriteLine();
+FindElement(array, position);
+
+void FindElement (int[,] arr , int position)
+{
+    if ( position < n*m)
+    {
+        System.Console.WriteLine($"Элемент на {position} позиции - > {arr[position / n, position % n]}");
+    }
+    else 
+    {
+        System.Console.WriteLine("такого элемента нет");
+    }
+}
+
+
+int[,] Generate2DArray (int m, int n)
+{
+    int[,] result = new int[m,n];
+    for ( int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(1,100);
+        }
+    }
+    return result;
+}
+
+void Print2DArray (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ web7hw7/task3/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+System.Console.WriteLine("Введите количество строк:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите количество столбцов:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = Generate2DArray(m,n);
+Print2DArray(array);
+System.Console.WriteLine();
+FindAverageOfColumns(array, m, n);
+
+int[,] Generate2DArray (int m, int n)
+{
+    int[,] result = new int[m,n];
+    for ( int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(1,10);
+        }
+    }
+    return result;
+}
+
+void Print2DArray (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindAverageOfColumns(int[,] arr, int m, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            sum += arr[j,i]; 
+        }
+        System.Console.Write($"{sum / m} ");
+        sum = 0;
+    }
+
+}
