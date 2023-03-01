20d21195fb52e7f87e06560b79cb8330ad701221
Liza <lizafedotova171197@gmail.com>
2023-03-02 00:44:16 +0300

    hw9

---
+++ c#_hw/web9hw9/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+System.Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(NatNumbers(N, 1));
+
+string NatNumbers (int start, int finish)
+{
+    if (start == finish)
+    {
+        return finish.ToString(); 
+    };
+    return (start + " " + NatNumbers(start - 1, finish));
+}
---
+++ c#_hw/web9hw9/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+
+System.Console.WriteLine("Введите M:");
+int M = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+System.Console.WriteLine(SumBetween(M, N));
+
+int SumBetween (int start, int finish)
+{
+    if (start == finish)
+    {
+        return finish; 
+    };
+    return (start + SumBetween (start + 1, finish));
+}
